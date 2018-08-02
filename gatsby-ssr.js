a86f32a1068675db6be0f19e3104d96e1067f331
Michael K. Clements <michaelkc@me.com>
2018-08-02 13:12:40 +0100

    Attempting ssr fixes

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,21 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { renderToString } from 'react-dom/server'
-import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 import store from './src/redux/store'
 
-exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString, setHeadComponents }) => {
-  const sheet = new ServerStyleSheet()
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
 
-  replaceBodyHTMLString(
-    renderToString(
-      <Provider store={store}>
-        <StyleSheetManager sheet={sheet.instance}>
-          {bodyComponent}
-        </StyleSheetManager>
-      </Provider>
-    )
+  const ConnectedBody = () => (
+    <Provider store={store}>
+      {bodyComponent}
+    </Provider>
   )
-
-  setHeadComponents([sheet.getStyleElement()])
+  replaceBodyHTMLString(renderToString(<ConnectedBody/>))
 }
